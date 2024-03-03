5cbc57e195ace328fef32c9949fdd6de2f209206
remdambrosio <rda7@shaw.ca>
2024-03-03 09:37:15 -0800

    added main loop and alternate mode

--- main.pl
+++ main.pl
@@ -1,35 +1,83 @@
 #!usr/bin/perl
 
-#Set replacement word
-sub replace {
-    return("big");
-}
+# SUBROUTINE: Embiggen text
+sub bigger {
+    my @bigger = @_;
+    for my $elem (@bigger) {
+        # If the element is a word (alphabetical characters only), replace it
+        if ($elem =~ /[a-zA-Z]/) {
+            # Store capitalization
+            my $capitalized = ($elem =~ /^[A-Z]/);
+            # Replace word
+            $elem = "big";
+            # Restore capitalization
+            $elem = ucfirst($elem) if $capitalized;
+        }
+    }
+    return @bigger;
+} # End bigger
 
-#Read input file
+# SUBROUTINE: Embetter text
+sub better {
+    my @better = @_;
+    for my $elem (@better) {
+        # If the element is a word (alphabetical characters only), replace it
+        if ($elem =~ /[a-zA-Z]/) {
+            # Store capitalization
+            my $capitalized = ($elem =~ /^[A-Z]/);
+            # Replace word
+            $elem = "bet";
+            # Restore capitalization
+            $elem = ucfirst($elem) if $capitalized;
+        }
+    }
+    return @better;
+} # End better
+
+
+# Open input file
 $file = "input.txt";
-open($fh, '<', $file) or die "Could not find $file";
+open($fh, '<', $file) or die "Could not find $file $!";
 $input = '';
+
+# Read line-by-line
 while (my $line = <$fh>) {
     $input .= $line;
 }
 close($fh);
 
-#Split input into array of words
-@split = split(/(\W+)/, $input);
+# Split input into array of words
+@input_words = split(/(\W+)/, $input);
 
-#Replace each word
-for my $elem (@split) {
-    #If the element is a word (alphabetical characters only), replace it
-    if ($elem =~ /[a-zA-Z]/) {
-        #Store capitalization
-        my $capitalized = ($elem =~ /^[A-Z]/);
-        #Replace word
-        $elem = replace($elem);
-        #Restore capitalization
-        $elem = ucfirst($elem) if $capitalized;
+# Main loop
+while () {
+
+    # Choose mode
+    my $mode = "unselected";
+    while ($mode ne "bigger" && $mode ne "better") {
+        print "Would you like to make your input.txt BIGGER or BETTER?\n";
+        $mode = lc(<STDIN>);
+        chomp($mode);
     }
-}
 
-#Concatenate into str and print
-$output = join('', @split);
-print "Your embiggened text is: $output";
+    # Copy input for manipulation
+    my @text = @input_words;
+
+    # Replace each word
+    if ($mode eq "bigger") {
+        @text = bigger(@text);
+    } elsif ($mode eq "better") {
+        @text = better(@text);
+    }
+
+    # Concatenate into str and print
+    my $output = join('', @text);
+    print "Your altered text is:\n====================\n$output\n====================\n";
+
+    # Repeat or quit
+    print "Would you like to try again? Y/N:\n";
+    my $quit_choice = lc(<STDIN>);
+    chomp($quit_choice);
+    if ($quit_choice eq "n") {last};
+
+} # End main loop
